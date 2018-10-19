e63b026fd85e6222749d01f42db7f77f7047cddf
kkanizaj <karla.kanizaj@gmail.com>
2018-10-19 12:28:40 +0200

    Updates

---
+++ precache-manifest.6d30a23dbcb3326a397b4b6f548af912.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d965181bad0ec2cd0799",
+    "url": "/kkanizaj.github.io/static/js/runtime~main.d965181b.js"
+  },
+  {
+    "revision": "8653d53e050f026d5a39",
+    "url": "/kkanizaj.github.io/static/js/main.8653d53e.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/kkanizaj.github.io/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "8653d53e050f026d5a39",
+    "url": "/kkanizaj.github.io/static/css/main.3a2c26b0.chunk.css"
+  },
+  {
+    "revision": "9620d2482c350ee032d7b7cc02f7fb6e",
+    "url": "/kkanizaj.github.io/index.html"
+  }
+];
